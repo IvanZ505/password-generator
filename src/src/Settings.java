af2b725d9091c7fc4cfa2a7281dcb8e984d9237b
izheng <zivan7644@gmail.com>
2022-08-18 15:22:30 -0400

    initial commit

---
+++ src/src/Settings.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Settings extends JPanel {
+    public Settings() {
+        Font myFont = new Font("Roboto", Font.PLAIN, 12);
+        this.setFont(myFont);
+        this.setFocusable(true);
+    }
+}
