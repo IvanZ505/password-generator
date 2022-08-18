c35eed0bc7b7901ebf10aca4d763fb13d92621f9
izheng <zivan7644@gmail.com>
2022-08-18 15:28:22 -0400

    added a slider

--- src/src/Settings.java
+++ src/src/Settings.java
@@ -6,5 +6,8 @@ public class Settings extends JPanel {
         Font myFont = new Font("Roboto", Font.PLAIN, 12);
         this.setFont(myFont);
         this.setFocusable(true);
+        JSlider slider = new JSlider();
+        slider.setPreferredSize(new Dimension(500, 20));
+        this.add(slider);
     }
 }
